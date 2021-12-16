b28ed3bb3851b12c0398739993c72f5ef5c71f05
lucasfarmwald <farmwaldlucas@gmail.com>
2021-12-16 00:22:28 -0600

    Done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,14 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const lineStyle = {
+    color: "firebrick",
+  }
+  return <div>
+    <div id="home"></div>
+      <h1 style={lineStyle}>{name}is a Web Developer from {city}</h1>
+    
+  </div>;
 }
 
 export default Home;
